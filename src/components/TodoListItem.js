3d64117754026609393f1b6e0a738d8c2aa0926c
LeeJH <ljhcplus@gmail.com>
2020-09-12 22:47:24 +0900

    Update TodoList Add, Remove, Toggle

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,19 +1,31 @@
-import React from "react";
-import { MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from "react-icons/md";
-import "./TodoListItem.scss";
+import React from 'react';
+import {
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames';
+import './TodoListItem.scss';
 
-const TodoListItem = () => {
-  return (
-    <div className="TodoListItem">
-      <div className="checkBox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
-      </div>
-      <div className="remove">
-        <MdRemoveCircleOutline />
-      </div>
-    </div>
-  );
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+    const { id, text, checked } = todo;
+
+    return (
+        <div className="TodoListItem">
+            <div
+                className={cn('checkBox', { checked })}
+                onClick={() => {
+                    onToggle(id);
+                }}
+            >
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                <div className="text">{text}</div>
+            </div>
+            <div className="remove" onClick={() => onRemove(id)}>
+                <MdRemoveCircleOutline />
+            </div>
+        </div>
+    );
 };
 
 export default TodoListItem;
